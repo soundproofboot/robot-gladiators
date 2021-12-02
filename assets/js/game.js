4135ed806dcc6dd7bcba186d4d01f50b348518bf
C Bares <colinedwinbares@gmail.com>
2021-12-02 14:45:39 -0600

    fixed blank/null player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -158,8 +158,17 @@ var randomNumber = function(min, max) {
   return value;
 }
 
+var getPlayerName = function() {
+  var name = "";
+  // loop
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+}
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
